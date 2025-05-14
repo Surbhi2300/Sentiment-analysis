376054851d6da354cf7acd8e46e81ead2da9dc4c
Surbhi2300 <aglawesurbhi@gmail.com>
2025-05-14 17:07:59 +0530

    realtimedetection.py

--- realtimedetection.py
+++ realtimedetection.py
@@ -32,6 +32,6 @@ while True:
             cv2.putText(im,'% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
         cv2.imshow("Output",im)
         cv2.waitKey(27)
-quit
+#quit #for closing the application 
     except cv2.error:
         pass        
